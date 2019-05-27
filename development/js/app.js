e4905f273f82d6b6d05773876604e3a78dc0b03b
Rafał Chełkowski <rchelek@gmail.com>
2019-05-27 18:13:19 +0200

    Zmieniam nazwę pliku helloform.scss na firstentry.scss. Dodaję reset.scss z podstawowym resetem styli, do pliku app.html dodaję ekran powitalny, jego funkcjonalość do pliku app.js i style do firstentry.scss. Dołączam czcionkę Open-sans do app.html przez @import w firstentry.scss - proponuję później to przenieść

--- development/js/app.js
+++ development/js/app.js
@@ -1,2 +1,23 @@
-console.log("U mnie działa;p");
-console.log("Tomek na króla");
+document.addEventListener('DOMContentLoaded', function () {
+
+    var submitName = document.getElementById('submit_name');          //Znajduję input imienia
+    var inputName = document.getElementById('input_name');            //Znajduję button wysyłający imię z inputu
+    var userName = document.getElementById('user_name');            //Imię w headerze aplikacji, póki co dorzuciłem je jako paragraf, żeby testować funkcjonalność. Jak Nikola skończy header, to poprawię
+    console.log(inputName.value);
+
+
+    submitName.addEventListener('click', function (event) {
+
+        if (inputName.value !== '') {                                               //jeśli coś jest w inpucie
+
+            localStorage.setItem('userName', inputName.value);                  //dodaję do local storage imię pod nazwą userName
+            // console.log(localStorage);
+            inputName.value = null;
+
+            userName.innerText = localStorage.userName;                        //zmieniam imię w html na podane przez input
+
+        }
+
+    })
+
+});
