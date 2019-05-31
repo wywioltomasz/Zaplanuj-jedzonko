863cd396e7fb6ef1d6bac862d86a509257e019e5
wywioltomasz <Arjuna.108>
2019-05-31 16:53:09 +0200

    usuwanie widgetow powiadomien

--- development/js/app.js
+++ development/js/app.js
@@ -40,4 +40,12 @@ document.addEventListener('DOMContentLoaded', function () {
     app_main.style.display = 'block';
     dashboard.style.display = 'none';
   }
+  //Usuwanie powiadomien widgetow//
+  var close_buttons = document.querySelectorAll('.fa-window-close');
+  for(var i=0; i < close_buttons.length; i++){
+    close_buttons[i].addEventListener('click', function () {
+      this.parentElement.parentElement.removeChild(this.parentElement);
+    })
+  }
+
 });
